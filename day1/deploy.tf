37fc6994d56be84a20494f72ce8ecc6aac10e61b
root <root@DESKTOP-NDHIA70.localdomain>
2026-03-02 19:32:05 -0300

    Half Day 1

---
+++ day1/deploy.tf
@@ -0,0 +1,53 @@
+// Last ubuntu version on east
+data "aws_ami" "ubuntu-east" {
+  most_recent = true
+  provider = aws.east
+ 
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  
+  owners = ["099720109477"]
+}
+
+// Last ubuntu version on west
+data "aws_ami" "ubuntu-west" {
+  most_recent = true
+  provider = aws.west
+ 
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  
+  owners = ["099720109477"]
+}
+
+// Resource on AWS East
+resource "aws_instance" "ubuntu_teste_east" {
+    provider = aws.east
+    ami = data.aws_ami.ubuntu-east.id
+    instance_type = "t3.micro"
+
+    tags = {
+        Name = "ec2-teste-ubuntu-e"
+    }
+}
+
+// Resource on AWS West
+resource "aws_instance" "ubuntu_teste_west" {
+    provider = aws.west
+    ami = data.aws_ami.ubuntu-west.id
+    instance_type = "t3.micro"
+
+    tags = {
+        Name = "ec2-teste-ubuntu-w"
+    }
+}
+
+# // Resource on GCP
+# resource "gcp_instance" "name" {
+#     provider = gcp.google
+#     name = "gcp-instance"
+# }
--- day1/main.tf
+++ day1/main.tf
@@ -4,15 +4,28 @@ terraform {
       source  = "hashicorp/aws"
       version = ">=5.87.0"
     }
+  #   gcp = {
+  #     source  = "hashicorp/google"
+  #     version = ">=5.87.0"
+  # }
   }
-
     backend "s3" {
     bucket = "descomplicando-terraform-rbaumann"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
 }
-
 provider "aws" { 
+  alias = east
   region = "us-east-1"   
 }
+
+provider "aws" { 
+  alias = west
+  region = "us-west-1"
+}
+
+# provider "gcp" {
+#   alias = google
+#   region = "us-central1"
+# }
