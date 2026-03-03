522a722a2adaa83f311cf460b453087f3c580096
root <root@DESKTOP-NDHIA70.localdomain>
2026-03-03 14:57:39 -0300

    Updating

--- day2/EC2/ec2.tf
+++ day2/EC2/ec2.tf
@@ -2,7 +2,13 @@ resource "aws_instance" "ubuntu" {
     count = var.servers
     ami = "ami-0b6c6ebed2801a5cb"
     instance_type = "t3.micro"
-    
+     credit_specification {
+      cpu_credits = "standard"
+    }
+    root_block_device {
+      volume_size = 20
+      volume_type = "gp3"
+    }
     tags = {
         Name = "ec2-teste-ubuntu"
     }
